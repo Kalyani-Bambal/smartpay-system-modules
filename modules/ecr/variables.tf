a34997369ee874ea6e24a4279f928e9087e174d4
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2026-05-19 09:26:59 +0000

    ecr is created

---
+++ modules/ecr/variables.tf
@@ -0,0 +1,13 @@
+variable "repository_name" {
+  type = string
+}
+
+variable "image_tag_mutability" {
+  type    = string
+  default = "MUTABLE"
+}
+
+variable "scan_on_push" {
+  type    = bool
+  default = true
+}
