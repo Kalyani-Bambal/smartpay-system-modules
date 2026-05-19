77682fcb7e22ae65fed79c07618833a1b592c91a
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2026-05-19 07:52:25 +0000

    rds module created

--- rds/main.tf
+++ rds/main.tf
@@ -1,5 +1,7 @@
 resource "aws_db_subnet_group" "this" {
-  name       = "${var.db_identifier}-subnet-group"
+
+  name = "${var.db_identifier}-subnet-group"
+
   subnet_ids = var.subnet_ids
 
   tags = {
@@ -8,21 +10,32 @@ resource "aws_db_subnet_group" "this" {
 }
 
 resource "aws_security_group" "rds_sg" {
+
   name        = "${var.db_identifier}-sg"
-  description = "RDS security group"
-  vpc_id      = var.vpc_id
+
+  description = "RDS Security Group"
+
+  vpc_id = var.vpc_id
 
   ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
+
+    from_port = var.db_port
+
+    to_port = var.db_port
+
+    protocol = "tcp"
+
     cidr_blocks = var.allowed_cidr_blocks
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+
+    from_port = 0
+
+    to_port = 0
+
+    protocol = "-1"
+
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -32,21 +45,38 @@ resource "aws_security_group" "rds_sg" {
 }
 
 resource "aws_db_instance" "this" {
-  identifier             = var.db_identifier
-  engine                 = var.engine
-  engine_version         = var.engine_version
-  instance_class         = var.instance_class
-  allocated_storage      = var.allocated_storage
-  db_name                = var.db_name
-  username               = var.username
-  password               = var.password
 
-  db_subnet_group_name   = aws_db_subnet_group.this.name
+  identifier = var.db_identifier
+
+  engine = var.engine
+
+  engine_version = var.engine_version
+
+  instance_class = var.instance_class
+
+  allocated_storage = var.allocated_storage
+
+  db_name = var.db_name
+
+  username = var.username
+
+  password = var.password
+
+  port = var.db_port
+
+  db_subnet_group_name = aws_db_subnet_group.this.name
+
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
   publicly_accessible = false
+
   skip_final_snapshot = true
-  multi_az            = var.multi_az
+
+  multi_az = var.multi_az
+
+  storage_encrypted = true
+
+  backup_retention_period = 7
 
   tags = {
     Name = var.db_identifier
