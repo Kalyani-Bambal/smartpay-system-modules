b9f79b87f916ed87a2e8c14c2f90d616ea65a05a
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2026-05-22 10:09:13 +0000

    duplicate delete

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -17,21 +17,6 @@ resource "aws_security_group" "rds_sg" {
 
   vpc_id = var.vpc_id
 
-  # Existing CIDR-based access
-  ingress {
-
-    from_port = var.db_port
-
-    to_port = var.db_port
-
-    protocol = "tcp"
-
-    cidr_blocks = var.allowed_cidr_blocks
-
-    security_groups = var.allowed_security_groups
-
-  }
-
   # Bastion Host Access
   ingress {
 
