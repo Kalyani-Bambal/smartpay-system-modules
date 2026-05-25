77362a3bb618181b5b547a06de8593e9f4f6b120
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2026-05-25 11:54:40 +0000

    update the modules

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -6,37 +6,52 @@ data "aws_eks_cluster_auth" "eks" {
   name = var.cluster_name
 }
 
+# Kubernetes Provider
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
+  cluster_ca_certificate = base64decode(
+    data.aws_eks_cluster.eks.certificate_authority[0].data
+  )
+  token = data.aws_eks_cluster_auth.eks.token
 }
 
+# Helm Provider
 provider "helm" {
-  kubernetes = {
-    host                   = var.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(var.eks_cluster_ca)
-    token                  = var.eks_cluster_token
+  kubernetes {
+    host = data.aws_eks_cluster.eks.endpoint
+
+    cluster_ca_certificate = base64decode(
+      data.aws_eks_cluster.eks.certificate_authority[0].data
+    )
+
+    token = data.aws_eks_cluster_auth.eks.token
   }
 }
 
-resource "kubernetes_namespace_v1" "argocd" {
+# ArgoCD Namespace
+resource "kubernetes_namespace" "argocd" {
   metadata {
     name = var.namespace
   }
 }
 
+# ArgoCD Helm Release
 resource "helm_release" "argocd" {
-  name       = "argocd"
-  namespace  = kubernetes_namespace_v1.argocd.metadata[0].name
+  name      = "argocd"
+
+  namespace = kubernetes_namespace.argocd.metadata[0].name
 
   repository = "https://argoproj.github.io/argo-helm"
 
-  chart      = "argo-cd"
+  chart   = "argo-cd"
 
-  version    = "7.3.8"
+  version = "7.3.8"
 
-  timeout    = 600
+  timeout = 600
+
+  depends_on = [
+    kubernetes_namespace.argocd
+  ]
 
   values = [
     <<EOF
--- modules/argocd/outputs.tf
+++ modules/argocd/outputs.tf
@@ -1,3 +1,7 @@
 output "argocd_namespace" {
-  value = kubernetes_namespace_v1.argocd.metadata[0].name
+  value = kubernetes_namespace.argocd.metadata[0].name
+}
+
+output "argocd_release_name" {
+  value = helm_release.argocd.name
 }
